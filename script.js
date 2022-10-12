6c2de7121495bc4db7f8584667c45e3d94ce8ab0
IsmailUmmer <ktismail92@gmail.com>
2022-10-12 13:21:07 +0530

    tested codes

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ const btn = document.querySelector('#btn');
 const seats = document.querySelectorAll('.row.seat:not(.booked');
 const total = document.getElementById('.total');
 
-// function booking (b, b1 ,b2){
-//     b.classList.toggle(b1);
-//     b.classList.toggle(b2);
-// }
-// function togglebooking (b ,b1,b2){
-//     booking(btn ,"selected", "booked");
-// }
 
 container.addEventListener('click',(e) => {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('booked')){
@@ -29,41 +22,3 @@ container.addEventListener('click',(e) => {
     }
 })
 
-// btn.onclick =() => {
-//     console.log(e.target);
-//  e.target.classList.toggle('booked');
-// }
-
-
-
-
-
-
-// btn.('click',(e)=> {
-//     if (e.target.classList.contains('selected')){
-//         e.target.classList.toggle('booked');
-
-//     }
-//     }
-//);
-
-
-// btn.onclick =() => {
-//     if (e.target.classList.contains('selected')){
-//              e.target.classList.toggle('booked');
-
-// }
-// }
-
-// container.addEventListener('click', (e) => {
-//     if (e.target.classList.contains('seat') && !e.target.classList.contains('booked')) {
-//         console.log(e.target);
-//         e.target.classList.toggle('selected');
-//      }   
-      
-//     if (btn.onclick = (e) => {
-//             if (e.target.classList.contains('selected')) {
-//                 e.target.classList.toggle('booked');
-
-//             })
-//     );
