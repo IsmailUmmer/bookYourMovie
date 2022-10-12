ce35fca05e4c05abd909603aee094bab546763ec
IsmailUmmer <ktismail92@gmail.com>
2022-10-12 22:02:07 +0530

    standerd price updated for btn

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 const container = document.querySelector('.container');
+const premiumContainer = document.querySelector('.premium-container');
 const btn = document.querySelector('#btn');
 const seats = document.querySelectorAll('.row.seat:not(.booked');
-const total = document.getElementById('.total');
+const total = document.getElementById('total');
 const standard = document.querySelector('.standard');
+const premium = document.querySelector('.premium')
+const standardPrice = document.getElementById('standard-price')
+
+function userSelectedseat(){
+    const selectedseats = document.querySelectorAll('.row .seat.selected');
+    // console.log(selectedseats);
+    const selectedseatcount = selectedseats.length;
+    // console.log(selectedseatcount);
+    total.innerText = selectedseatcount*120;
+}
+
+premiumContainer.addEventListener('click',(e) => {
+    if (e.target.classList.contains('premium-seat') && !e.target.classList.contains('booked')){
+        // console.log(e.target);
+        e.target.classList.toggle('selected');
+    }
+
+}   
+);
+
+premium.addEventListener('click',(e) => {
+    if (e.target.classList.contains('premium-seat') && !e.target.classList.contains('booked')){
+        btn.onclick =() => {
+        //  e.target.classList.toggle(togglebooking());
+         e.target.classList.replace ("selected", "booked");
+        }
+
+    }
+});
+
 //    for selection 
 container.addEventListener('click',(e) => {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('booked')){
         // console.log(e.target);
         e.target.classList.toggle('selected');
     }
+    userSelectedseat();
 }   
 );
 // when clicked paybtn 
